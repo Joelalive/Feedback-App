39e0ac3f2e214769843e6a6fb2a9f2bc97d0cec4
joelalive <joelimalive1994@gmail.com>
2022-10-04 23:11:40 +0530

    header change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 function App() {
-  const title = "Blog";
-  const body = "Blog context";
-  const comments = [
-    { id: 1, text: "comment 1" },
-    { id: 2, text: "comment 2" },
-    { id: 3, text: "comment 3" },
-  ];
   return (
     <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+      <h1>My App</h1>
     </div>
   );
 }
